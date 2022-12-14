01cec696b3a5a555ef5a02cadc934f2af9487087
rm-bias <sandhya.at.18@gmail.com>
2022-12-14 12:28:38 +0530

    Added piece.java file which is a superclass for each piece in the chess game

---
+++ Mini_project/src/Mini_project/Piece.java
@@ -0,0 +1,16 @@
+package Mini_project;
+
+public class Piece {
+	private Color color;
+	private Board board;
+	private Location location;
+
+	public Piece() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Location getLocation() {
+		
+	}
+
+}
